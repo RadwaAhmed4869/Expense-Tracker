895fff0a94df829fa134dae82806cafd2260ae13
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-04-28 01:10:42 +0300

    Event listeners and State
    (002 -> 005)
    Listening to Events & Event Handlers
    How Component Functions Are Executed
    _State_ & _useState_ Hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
+  // setTimeout(() => {
+  //   setAmount("$$$");
+  // }, 3000);
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
