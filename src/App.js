63fa49f93dfeef99950bb5cae6a1393bbd1ef643
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-04-28 01:31:10 +0300

    007 Adding form inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import NewExpense from "./components/NewExpenses/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -39,10 +40,10 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses}/>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
