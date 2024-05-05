1fede09b4709ef38e592f23bf057e1226f7fd01b
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-05-05 12:25:00 +0300

    expenses chart
    007 Demo App_ Adding a Chart
    008 Adding Dynamic Styles

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected={filterYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList list={filteredExpenses} />
     </Card>
   );
